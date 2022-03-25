7ec13e88cb93f232b771d38563c128bdc062b6d3
jiamid <jiamid@qq.com>
2022-03-25 17:36:48 +0800

    init

--- src/main_api.py
+++ src/main_api.py
@@ -3,12 +3,10 @@ from starlette.middleware.cors import CORSMiddleware
 from uvicorn import run
 from commons.config import system_conf
 from api.crawler_api import insert_router
-from api.gs_api import gs_router
-from api.test_api import test_router
 
-API_HEADERS = '/winte_olympics_api'
+API_HEADERS = '/cykl_crawlab'
 
-app = FastAPI(title='Winter Olympics Api', openapi_url=f'{API_HEADERS}/openapi.json')
+app = FastAPI(title='CYKL Crawlab API', openapi_url=f'{API_HEADERS}/openapi.json')
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,13 +16,8 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-if system_conf.CRAWLER_API_STATUS == 'true':
-    app.include_router(insert_router, prefix=API_HEADERS)
-if system_conf.GS_API_STATUS == 'true':
-    app.include_router(gs_router, prefix=API_HEADERS)
 
-if system_conf.TEST_API_STATUS == 'true':
-    app.include_router(test_router)
+app.include_router(insert_router, prefix=API_HEADERS)
 
 if __name__ == '__main__':
     run(app, host='0.0.0.0', port=system_conf.API_PORT)
